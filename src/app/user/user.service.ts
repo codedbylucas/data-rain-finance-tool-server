eb512bba818d75f8078e4a1e9c22341af71a663b
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-11-30 22:29:58 -0300

    feat(user): ensure createUser return bad request if role is invalid

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersEntity } from './entities/user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
@@ -12,9 +13,19 @@ export class UserService {
   ) {}
 
   async createUser(data: CreateUserDto): Promise<UsersEntity> {
+    if (!this.verifyRole(data.role)) {
+      throw new BadRequestException(`Role '${data.role}' is invalid`);
+    }
     const user: UsersEntity = this.userRepository.create(data);
     const savedUser: UsersEntity = await this.userRepository.save(user);
 
     return savedUser;
+  }
+
+  verifyRole(role: string): boolean {
+    if (role !== 'preSale' && role !== 'financial') {
+      return false;
+    }
+    return true;
   }
 }
