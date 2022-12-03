5c842bc00c319067c3731d0abea84699e825bd32
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-12-02 21:08:35 -0300

    feat: ensure server errors are handled

--- src/app/auth/criptography/bcrypt/bcrypt.adapter.ts
+++ src/app/auth/criptography/bcrypt/bcrypt.adapter.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { serverError } from 'src/app/util/server-error';
 
 @Injectable()
 export class BcryptAdapter {
   async hash(value: string, salt: number): Promise<string> {
-    const hash = await bcrypt.hash(value, salt);
+    const hash = await bcrypt.hash(value, salt).catch(serverError);
     return hash;
   }
 
   async compare(value: string, hash: string): Promise<boolean> {
-    const isValid = await bcrypt.compare(value, hash);
+    const isValid = await bcrypt.compare(value, hash).catch(serverError);
     return isValid;
   }
 }
--- src/app/user/repositories/user.repository.ts
+++ src/app/user/repositories/user.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { serverError } from 'src/app/util/server-error';
 import { Repository } from 'typeorm';
 import { UserEntity } from '../entities/user.entity';
 import { CreateUserDto } from '../service/dto/create-user.dto';
@@ -13,21 +14,27 @@ export class UserRepository {
 
   async createUser(data: CreateUserDto): Promise<UserEntity> {
     const createdUser: UserEntity = this.userRepository.create(data);
-    const savedUser: UserEntity = await this.userRepository.save(createdUser);
+    const savedUser: UserEntity = await this.userRepository
+      .save(createdUser)
+      .catch(serverError);
     return savedUser;
   }
 
   async findUserByEmail(email: string): Promise<UserEntity> {
-    const userOrNull = await this.userRepository.findOne({
-      where: { email },
-    });
+    const userOrNull = await this.userRepository
+      .findOne({
+        where: { email },
+      })
+      .catch(serverError);
     return userOrNull;
   }
 
   async findUserById(id: string): Promise<UserEntity> {
-    const userOrNull = await this.userRepository.findOne({
-      where: { id },
-    });
+    const userOrNull = await this.userRepository
+      .findOne({
+        where: { id },
+      })
+      .catch(serverError);
     return userOrNull;
   }
 }
