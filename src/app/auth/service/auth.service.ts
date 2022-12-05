ea856a27c52508bbbb22f67c765ad6ce761be153
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-12-05 09:44:47 -0300

    refactor(auth): remove either

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { Either, left, rigth } from 'src/app/shared/either/either';
 import { UserEntity } from 'src/app/user/entities/user.entity';
 import { UserRepository } from 'src/app/user/repositories/user.repository';
 import { BcryptAdapter } from '../criptography/bcrypt/bcrypt.adapter';
@@ -15,35 +14,29 @@ export class AuthService {
     private readonly jwtAdapter: JwtAdapter,
   ) {}
 
-  async validateUser(
-    dto: LoginDto,
-  ): Promise<Either<UnauthorizedException, UserEntity>> {
+  async validateUser(dto: LoginDto): Promise<UserEntity> {
     const { password, email } = dto;
     const userOrNull = await this.userRepository.findUserByEmail(email);
     if (!userOrNull) {
-      return left(new UnauthorizedException('Invalid email and/or password!'));
+      throw new UnauthorizedException('Invalid email and/or password!');
     }
 
     const isPasswordValid = await this.bcryptAdapter.compare(
       password,
       userOrNull.password,
     );
-
     if (!isPasswordValid) {
-      return left(new UnauthorizedException('Invalid email and/or password!'));
+      throw new UnauthorizedException('Invalid email and/or password!');
     }
 
-    return rigth(userOrNull);
+    return userOrNull;
   }
 
   async login(dto: LoginDto): Promise<LoginResponse> {
     const userOrError = await this.validateUser(dto);
-    if (userOrError.isLeft()) {
-      throw userOrError.value;
-    }
 
     const userIdEncrypted = await this.jwtAdapter.encrypt(
-      userOrError.value.id,
+      userOrError.id,
       process.env.JWT_SECRET,
     );
 
