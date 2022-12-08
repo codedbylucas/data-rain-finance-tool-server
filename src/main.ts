0a6549930114afb9873a32290d9a2ae7d4daec7f
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-12-07 21:55:03 -0300

    feat: make the client folder available to the client

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  app.useStaticAssets(join(__dirname, '..', 'client'));
   const config = new DocumentBuilder()
     .setTitle('Finance Tool Server')
     .setDescription('Aplicação de previsão orçamentária')
