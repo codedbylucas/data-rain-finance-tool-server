aafc6444f8fc29fb7d151a3cb60519add95a2c73
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-12-09 19:19:53 -0300

    feat: cors true

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
