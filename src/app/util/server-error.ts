1926dd38edb59ad00406f274277b28f0af046ae7
Lucas Marques <lucasmsantos819@gmail.com>
2022-12-17 16:37:01 -0300

    feat: add console log error

--- src/app/util/server-error.ts
+++ src/app/util/server-error.ts
@@ -3,11 +3,7 @@ import { InternalServerErrorException } from '@nestjs/common';
 export function serverError(error: Error): undefined {
   const errorLines = error.message?.split('\n');
   const lastErrorLine = errorLines[errorLines.length - 1].trim();
-
-  if (!lastErrorLine) {
-    console.log(error);
-  }
-
+  console.log(error);
   throw new InternalServerErrorException(
     lastErrorLine || 'Oops, something happened.',
   );
