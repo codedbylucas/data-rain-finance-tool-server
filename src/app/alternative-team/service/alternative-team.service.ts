d96796e235b2d305e61ecf8098866940b36326f4
Lucas Marques <lucasmsantos819@gmail.com>
2022-12-18 17:07:29 -0300

    feat(alternative-team): implement `findAlternativeTeamByIds`

--- src/app/alternative-team/service/alternative-team.service.ts
+++ src/app/alternative-team/service/alternative-team.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { AlternativeService } from 'src/app/alternatives/service/alternative.service';
 import { TeamService } from 'src/app/team/service/team.service';
 import { createUuid } from 'src/app/util/create-uuid';
+import { AlternativeTeamEntity } from '../entities/alternative-team.entity';
 import { UpdateAlternativeTeamProps } from '../protocols/props/update-alternative-team.props';
 import { UpdateAlternativeTeamResponse } from '../protocols/update-alternative-team-response';
 import { AlternativeTeamRepository } from '../repositories/alternative-team.repository';
@@ -16,18 +17,16 @@ export class AlternativeTeamService {
   ) {}
 
   async createAlternativeTeam(dto: CreateAlternativeTeamDto): Promise<void> {
-    await this.alternativeService.verifyAlternativeExist(dto.alternativeId);
-    await this.teamService.verifyTeamExist(dto.teamId);
+    await this.verifyAlternativeAndTeamExist(dto.alternativeId, dto.teamId);
 
-    const alternativeTeamOrNull =
-      await this.alternativeTeamRepository.findAlternativeTeamByIds(
-        dto.alternativeId,
-        dto.teamId,
-      );
-
+    const alternativeTeamOrNull = await this.findAlternativeTeamByIds(
+      dto.alternativeId,
+      dto.teamId,
+    );
     if (alternativeTeamOrNull) {
       throw new BadRequestException('This relationship already exists');
     }
+
     await this.alternativeTeamRepository.createTeamAlternative({
       ...dto,
       id: createUuid(),
@@ -37,14 +36,11 @@ export class AlternativeTeamService {
   async updateAlternativeTeamByIds(
     props: UpdateAlternativeTeamProps,
   ): Promise<UpdateAlternativeTeamResponse> {
-    await this.alternativeService.verifyAlternativeExist(props.alternativeId);
-    await this.teamService.verifyTeamExist(props.teamId);
-    const alternativeTeamOrNull =
-      await this.alternativeTeamRepository.findAlternativeTeamByIds(
-        props.alternativeId,
-        props.teamId,
-      );
-
+    await this.verifyAlternativeAndTeamExist(props.alternativeId, props.teamId);
+    const alternativeTeamOrNull = await this.findAlternativeTeamByIds(
+      props.alternativeId,
+      props.teamId,
+    );
     if (!alternativeTeamOrNull) {
       throw new BadRequestException(
         `It was not possible to find a relationship between ${props.alternativeId} and ${props.teamId}`,
@@ -58,5 +54,26 @@ export class AlternativeTeamService {
       teamId: alternativeTeamUpdated.teamId,
       workHours: alternativeTeamUpdated.workHours,
     };
+  }
+
+  async verifyAlternativeAndTeamExist(
+    alternativeId: string,
+    teamId: string,
+  ): Promise<void> {
+    await this.alternativeService.verifyAlternativeExist(alternativeId);
+    await this.teamService.verifyTeamExist(teamId);
+  }
+
+  async findAlternativeTeamByIds(
+    alternativeId: string,
+    teamId: string,
+  ): Promise<AlternativeTeamEntity> {
+    const alternativeTeamOrNull =
+      await this.alternativeTeamRepository.findAlternativeTeamByIds(
+        alternativeId,
+        teamId,
+      );
+
+    return alternativeTeamOrNull;
   }
 }
