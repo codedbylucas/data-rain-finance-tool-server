3cd75ff7b66cfa2da15a3e761981d5f540e9386d
Lucas Marques <lucasmsantos819@gmail.com>
2022-12-20 15:19:32 -0300

    feat(client): change format companyName and phone

--- src/app/client/service/client.service.ts
+++ src/app/client/service/client.service.ts
@@ -9,6 +9,9 @@ export class ClientService {
   constructor(private readonly clientRepository: ClientRepository) {}
 
   async createClient(dto: CreateClientDto): Promise<CreateClienteResponse> {
+    dto.companyName = dto.companyName.toLowerCase().trim();
+    dto.phone = dto.phone.replace(/\s/g, '').replace(/[^0-9]/g, '');
+
     const clientCreated = await this.clientRepository.createClient({
       ...dto,
       id: createUuid(),
