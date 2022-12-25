8b57c17d01bee34655f5493092df6408da044baf
Lucas Marques <lucasmsantos819@gmail.com>
2022-12-25 13:23:25 -0300

    feat(role): add ApiTags

--- src/app/role/role.controller.ts
+++ src/app/role/role.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { FindRoleResponse } from './protocols/find-role-response';
 import { RoleService } from './service/role.service';
 
 @Controller('role')
+@ApiTags('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
