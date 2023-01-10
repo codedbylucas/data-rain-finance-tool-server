ca8b8cfbfbede819d8ba4c072e5dd4e49ff87fc8
Lucas Marques <lucasmsantos819@gmail.com>
2023-01-10 20:25:31 -0300

    feat(client): insert `apiOperation`

--- src/app/client/client.controller.ts
+++ src/app/client/client.controller.ts
@@ -9,7 +9,7 @@ import {
   ParseUUIDPipe,
   Post,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateClienteResponse } from './protocols/create-client-response';
 import { FindAllClientsResponse } from './protocols/find-all-clients-response';
 import { FindClientByIdResponse } from './protocols/find-client-by-id-response';
@@ -23,6 +23,9 @@ export class ClientController {
   constructor(private readonly clientService: ClientService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Create a client',
+  })
   async createClient(
     @Body() dto: CreateClientDto,
   ): Promise<CreateClienteResponse> {
@@ -35,11 +38,17 @@ export class ClientController {
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Find all clients',
+  })
   async findAllClients(): Promise<FindAllClientsResponse[]> {
     return await this.clientService.findAllClients();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Find client by id',
+  })
   async findClientById(
     @Param('id', new ParseUUIDPipe()) id: string,
   ): Promise<FindClientByIdResponse> {
@@ -47,6 +56,9 @@ export class ClientController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete client by id',
+  })
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteClientById(
     @Param('id', new ParseUUIDPipe()) id: string,
