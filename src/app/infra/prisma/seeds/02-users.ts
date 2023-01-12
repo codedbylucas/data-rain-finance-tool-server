b3ef0e81cfe0b158636d1ac7e2eaaa61b4e13bd7
Lucas Marques <lucasmsantos819@gmail.com>
2023-01-12 18:35:08 -0300

    feat: implement user seed

---
+++ src/app/infra/prisma/seeds/02-users.ts
@@ -0,0 +1,80 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+
+export const users = async (): Promise<Prisma.UsersCreateInput[]> => {
+  const users: Prisma.UsersCreateInput[] = [
+    {
+      name: 'Admin',
+      email: 'admin@admin.com',
+      password: 'Admin@1234',
+      imageUrl: `/default-profile-picture/default-profile-picture.jpg`,
+      billable: false,
+      position: 'Admin',
+      allocated: false,
+      role: {
+        connect: {
+          name: 'admin',
+        },
+      },
+    },
+    {
+      name: 'User 01',
+      email: 'user01@mail.com',
+      password: 'Abcd@1234',
+      imageUrl: `/default-profile-picture/default-profile-picture.jpg`,
+      billable: false,
+      position: 'Dev Ops',
+      allocated: false,
+      role: {
+        connect: {
+          name: 'profissional_services',
+        },
+      },
+    },
+    {
+      name: 'User 02',
+      email: 'user02@mail.com',
+      password: 'Abcd@1234',
+      imageUrl: `/default-profile-picture/default-profile-picture.jpg`,
+      billable: false,
+      position: 'Back-end developer',
+      allocated: false,
+      role: {
+        connect: {
+          name: 'profissional_services',
+        },
+      },
+    },
+    {
+      name: 'User 03',
+      email: 'user03@mail.com',
+      password: 'Abcd@1234',
+      imageUrl: `/default-profile-picture/default-profile-picture.jpg`,
+      billable: false,
+      position: 'Front-end developer',
+      allocated: false,
+      role: {
+        connect: {
+          name: 'profissional_services',
+        },
+      },
+    },
+  ];
+  return users;
+};
+
+export const user = async (prisma: PrismaClient) => {
+  for (const obj of Object.values(await users())) {
+    obj.id = uuid();
+    obj.password = await bcrypt.hash(obj.password, 12);
+
+    await prisma.users.upsert({
+      where: { id: 'id' },
+      update: {},
+      create: {
+        ...obj,
+      },
+    });
+  }
+};
