6f6fc2ff1b869ac5de9e31d9555b91daefacc3de
Lucas Marques <lucasmsantos819@gmail.com>
2023-01-19 17:36:46 -0300

    feat: remove client name

--- src/app/infra/prisma/seeds/05-clients.ts
+++ src/app/infra/prisma/seeds/05-clients.ts
@@ -4,45 +4,39 @@ import { v4 as uuid } from 'uuid';
 export const clients: Prisma.ClientsCreateInput[] = [
   {
     id: uuid(),
-    name: 'Lucas Marques',
     companyName: 'company 01',
     email: 'company01@mail.com',
-    phone: '(11) 9 9933-9933',
+    phone: '11999339933',
   },
   {
     id: uuid(),
-    name: 'João Pedro Thuler',
     companyName: 'company 02',
     email: 'company02@mail.com',
-    phone: '(11) 9 9923-9933',
+    phone: '11999339933',
   },
   {
     id: uuid(),
-    name: 'Max Cleiton',
     companyName: 'company 03',
     email: 'company03@mail.com',
-    phone: '(11) 9 9913-9933',
+    phone: '11999339933',
   },
   {
     id: uuid(),
-    name: 'Arlyson Mathias',
     companyName: 'company 04',
     email: 'company04@mail.com',
-    phone: '(11) 9 9213-9933',
+    phone: '11999339933',
   },
   {
     id: uuid(),
-    name: 'Alexandre Silvestre',
     companyName: 'company 05',
     email: 'company05@mail.com',
-    phone: '(11) 9 9113-9933',
+    phone: '11999339933',
   },
   {
     id: uuid(),
-    name: 'Vinícius Oliveira Gonçalves',
     companyName: 'company 06',
     email: 'company06@mail.com',
-    phone: '(11) 9 9113-9933',
+    phone: '11999339933',
   },
 ];
 
