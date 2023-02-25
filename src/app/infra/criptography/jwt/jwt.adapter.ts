285a81eb0e0be67d2d1f66e0a7d26a3a479e97cc
Lucas Marques <lucasmsantos819@gmail.com>
2023-02-25 11:56:20 -0300

    feat(JwtAdapter): implementation `verifyToken`

--- src/app/infra/criptography/jwt/jwt.adapter.ts
+++ src/app/infra/criptography/jwt/jwt.adapter.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+
+export interface DecodedToken {
+  userId: string;
+}
 
 @Injectable()
 export class JwtAdapter {
   constructor(private readonly jwtService: JwtService) {}
+
   async encrypt(value: string): Promise<string> {
     const accessToken = await this.jwtService.sign({ userId: value });
     return accessToken;
+  }
+
+  async verifyToken(accessToken: string): Promise<DecodedToken> {
+    try {
+      const decoded = (await this.jwtService.decode(
+        accessToken,
+      )) as DecodedToken;
+
+      const userId = decoded.userId;
+      return { userId };
+    } catch (err) {
+      throw new BadRequestException(`Invalid Token`);
+    }
   }
 }
