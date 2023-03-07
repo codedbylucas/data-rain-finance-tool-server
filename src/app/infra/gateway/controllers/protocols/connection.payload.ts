a32147962e45437d9a528e6fdaafed8cd5e0e23a
Lucas Marques <lucasmsantos819@gmail.com>
2023-03-07 16:28:10 -0300

    feat(gateway): implementation `sendConnectionStatus`

--- src/app/infra/gateway/controllers/protocols/connection.payload.ts
+++ src/app/infra/gateway/controllers/protocols/connection.payload.ts
@@ -2,3 +2,9 @@ export interface ConnectionPayload {
   status: boolean;
   message: string;
 }
+
+export interface SendConnectionPayload {
+  clientId: string;
+  status: boolean;
+  message: string;
+}
