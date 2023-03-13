69d9e93692254589863bc28c1c93aba92622ff92
Lucas Marques <lucasmsantos819@gmail.com>
2023-03-13 16:23:23 -0300

    feat: add `timeZone` to functions that form hours

--- src/app/util/formatted-current-date.ts
+++ src/app/util/formatted-current-date.ts
@@ -2,6 +2,7 @@ export const formattedCurrentDate = (dateTime: Date): string => {
   if (!dateTime) {
     return null;
   }
-  const formattedDate = dateTime.toLocaleDateString('pt-br');
+  const options = { timeZone: 'America/Sao_Paulo' };
+  const formattedDate = dateTime.toLocaleDateString('pt-br', options);
   return formattedDate;
 };
